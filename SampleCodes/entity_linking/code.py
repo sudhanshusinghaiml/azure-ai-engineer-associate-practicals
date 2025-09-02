5ec60e82d02bbea84defb081ff5172bb4f64cd67
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2025-09-03 01:51:12 +0530

    Added Entity Linking

---
+++ SampleCodes/entity_linking/code.py
@@ -0,0 +1,51 @@
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+language_key = os.environ.get('LANGUAGE_KEY')
+language_endpoint = os.environ.get('LANGUAGE_ENDPOINT')
+
+# Authenticate the client using your key and endpoint 
+def authenticate_client():
+    ta_credential = AzureKeyCredential(language_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=language_endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+# Example function for recognizing entities and providing a link to an online data source.
+def entity_linking_example(client):
+
+    try:
+        documents = ["""Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, 
+        to develop and sell BASIC interpreters for the Altair 8800. 
+        During his career at Microsoft, Gates held the positions of chairman,
+        chief executive officer, president and chief software architect, 
+        while also being the largest individual shareholder until May 2014."""]
+        result = client.recognize_linked_entities(documents = documents)[0]
+
+        print("Linked Entities:\n")
+        for entity in result.entities:
+            print("\tName: ", entity.name, "\tId: ", entity.data_source_entity_id, "\tUrl: ", entity.url,
+            "\n\tData Source: ", entity.data_source)
+            print("\tMatches:")
+            for match in entity.matches:
+                print("\t\tText:", match.text)
+                print("\t\tConfidence Score: {0:.2f}".format(match.confidence_score))
+                print("\t\tOffset: {}".format(match.offset))
+                print("\t\tLength: {}".format(match.length))
+            
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+if __name__ == "__main__":
+    entity_linking_example(client)
+
--- SampleCodes/entity_linking/script.sh
+++ SampleCodes/entity_linking/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./SampleCodes/key_phrase_extraction/script.sh
+# ./SampleCodes/entity_linking/script.sh
 
 # LANGUAGE_ENDPOINT = https://mslearn-azureai-foundry.cognitiveservices.azure.com/"
 # LANGUAGE_ENDPOINT = https://{resource_name}.cognitiveservices.azure.com/" 
@@ -8,5 +8,5 @@
 curl -X POST $LANGUAGE_ENDPOINT/language/:analyze-text?api-version=2022-05-01 \
 -H "Content-Type: application/json" \
 -H "Ocp-Apim-Subscription-Key: $LANGUAGE_KEY" \
--d "@./SampleCodes/key_phrase_extraction/kpe_payload.json" \
--o "./SampleCodes/key_phrase_extraction/response.json"
+-d "@./SampleCodes/entity_linking/payload.json" \
+-o "./SampleCodes/entity_linking/response.json"
