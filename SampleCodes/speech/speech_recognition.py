fc73417e8c3a054f793cdb2803ec3b1562331d2a
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2025-09-04 03:52:30 +0530

    Completed Speech Codes Testing

--- SampleCodes/speech/speech_recognition.py
+++ SampleCodes/speech/speech_recognition.py
@@ -16,7 +16,8 @@ def recognize_from_microphone():
 
     speech_config = speechsdk.SpeechConfig(
         subscription=os.environ.get('SPEECH_KEY'), 
-        endpoint=os.environ.get('SPEECH_ENDPOINT'))
+        endpoint=os.environ.get('SPEECH_ENDPOINT'),
+        region = "eastus2")
     
     speech_config.speech_recognition_language="en-US"
 
@@ -39,5 +40,46 @@ def recognize_from_microphone():
             print("Did you set the speech resource key and endpoint values?")
 
 
+def recognize_from_mic():
+    speech_config = speechsdk.SpeechConfig(
+        subscription=os.environ.get('SPEECH_KEY'), 
+        endpoint=os.environ.get('STT_SPEECH_ENDPOINT')
+        # region = "eastus2"
+    )
+    
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+    print("Speak into your microphone.")
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+    return speech_recognition_result
+
+
+def recognize_from_file():
+    speech_config = speechsdk.SpeechConfig(
+        subscription=os.environ.get('SPEECH_KEY'), 
+        endpoint=os.environ.get('SPEECH_ENDPOINT')
+        # region = "eastus2"
+    ) 
+    audio_config = speechsdk.AudioConfig(filename= "SampleCodes/speech/audio.wav")
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+    return speech_recognition_result
+
+
 if __name__ == "__main__":
-    recognize_from_microphone()
+    speech_recognition_result = recognize_from_mic()
+    # speech_recognition_result = recognize_from_file()
+
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(speech_recognition_result.text))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+
+
