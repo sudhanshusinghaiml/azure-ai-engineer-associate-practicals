4f02c931dfdfedda845d76719499270bd2de6044
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2025-09-07 00:57:33 +0530

    Added content safety logic

---
+++ AzureAIContentSafety/code/analyze_image_async.py
@@ -0,0 +1,60 @@
+import asyncio
+import os
+from azure.ai.contentsafety.aio import ContentSafetyClient
+from azure.ai.contentsafety.models import ImageCategory
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+from azure.ai.contentsafety.models import AnalyzeImageOptions, ImageData
+
+
+async def analyze_image_async():
+    # [START analyze_image_async]
+
+    key = os.environ["CONTENT_SAFETY_KEY"]
+    endpoint = os.environ["CONTENT_SAFETY_ENDPOINT"]
+    image_path = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "./sample_data/image.jpg"))
+
+    # Create a Content Safety client
+    client = ContentSafetyClient(endpoint, AzureKeyCredential(key))
+
+    async with client:
+        # Build request
+        with open(image_path, "rb") as file:
+            request = AnalyzeImageOptions(image=ImageData(content=file.read()))
+
+        # Analyze image
+        try:
+            response = await client.analyze_image(request)
+        except HttpResponseError as e:
+            print("Analyze image failed.")
+            if e.error:
+                print(f"Error code: {e.error.code}")
+                print(f"Error message: {e.error.message}")
+                raise
+            print(e)
+            raise
+
+    hate_result = next(item for item in response.categories_analysis if item.category == ImageCategory.HATE)
+    self_harm_result = next(item for item in response.categories_analysis if item.category == ImageCategory.SELF_HARM)
+    sexual_result = next(item for item in response.categories_analysis if item.category == ImageCategory.SEXUAL)
+    violence_result = next(item for item in response.categories_analysis if item.category == ImageCategory.VIOLENCE)
+
+    if hate_result:
+        print(f"Hate severity: {hate_result.severity}")
+    if self_harm_result:
+        print(f"SelfHarm severity: {self_harm_result.severity}")
+    if sexual_result:
+        print(f"Sexual severity: {sexual_result.severity}")
+    if violence_result:
+        print(f"Violence severity: {violence_result.severity}")
+
+    # [END analyze_image_async]
+
+
+async def main():
+    await analyze_image_async()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ AzureAIContentSafety/code/entra_id_auth.py
@@ -0,0 +1,56 @@
+import os
+from azure.ai.contentsafety import ContentSafetyClient
+from azure.ai.contentsafety.models import TextCategory
+from azure.core.exceptions import HttpResponseError
+from azure.ai.contentsafety.models import AnalyzeTextOptions
+from azure.identity import DefaultAzureCredential
+
+
+def analyze_text_with_entra_id_auth():
+    # [START analyze_text]
+
+
+
+    endpoint = os.environ["CONTENT_SAFETY_ENDPOINT"]
+
+    """DefaultAzureCredential will use the values from these environment
+    variables: AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET
+    """
+    credential = DefaultAzureCredential()
+    # Create a Content Safety client
+    client = ContentSafetyClient(endpoint, credential)
+
+    # Construct a request
+    request = AnalyzeTextOptions(text="You are an idiot")
+
+    # Analyze text
+    try:
+        response = client.analyze_text(request)
+    except HttpResponseError as e:
+        print("Analyze text failed.")
+        if e.error:
+            print(f"Error code: {e.error.code}")
+            print(f"Error message: {e.error.message}")
+            raise
+        print(e)
+        raise
+
+    hate_result = next(item for item in response.categories_analysis if item.category == TextCategory.HATE)
+    self_harm_result = next(item for item in response.categories_analysis if item.category == TextCategory.SELF_HARM)
+    sexual_result = next(item for item in response.categories_analysis if item.category == TextCategory.SEXUAL)
+    violence_result = next(item for item in response.categories_analysis if item.category == TextCategory.VIOLENCE)
+
+    if hate_result:
+        print(f"Hate severity: {hate_result.severity}")
+    if self_harm_result:
+        print(f"SelfHarm severity: {self_harm_result.severity}")
+    if sexual_result:
+        print(f"Sexual severity: {sexual_result.severity}")
+    if violence_result:
+        print(f"Violence severity: {violence_result.severity}")
+
+    # [END analyze_text]
+
+
+if __name__ == "__main__":
+    analyze_text_with_entra_id_auth()
